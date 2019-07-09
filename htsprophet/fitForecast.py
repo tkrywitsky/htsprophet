180989f4c0e678b22481d6716339aca59dbb8041
Tom Krywitsky <tkrywit@gmail.com>
2019-07-09 15:51:39 -0600

    Use multiprocessing for prophet forecast

--- htsprophet/fitForecast.py
+++ htsprophet/fitForecast.py
@@ -20,6 +20,99 @@ import numpy as np
 from fbprophet import Prophet
 import contextlib, os
 from scipy.special import inv_boxcox
+import multiprocessing
+
+def fitInvididualForecast(node_params):
+    node = node_params["node"]
+    y = node_params["y"]
+    cap = node_params["cap"]
+    capF = node_params["capF"]
+
+    changepoints = node_params["changepoints"]
+    n_changepoints = node_params["n_changepoints"]
+    yearly_seasonality = node_params["yearly_seasonality"]
+    weekly_seasonality = node_params["weekly_seasonality"]
+    daily_seasonality = node_params["daily_seasonality"]
+    holidays = node_params["holidays"]
+    seasonality_prior_scale = node_params["seasonality_prior_scale"]
+    holidays_prior_scale = node_params["holidays_prior_scale"]
+    changepoint_prior_scale = node_params["changepoint_prior_scale"]
+    mcmc_samples = node_params["mcmc_samples"]
+    interval_width = node_params["interval_width"]
+    uncertainty_samples = node_params["uncertainty_samples"]
+
+    h = node_params["h"]
+    freq = node_params["freq"]
+    include_history = node_params["include_history"]
+
+    nodeToForecast = pd.concat([y.iloc[:, [0]], y.iloc[:, node+1]], axis = 1)
+    if isinstance(cap, pd.DataFrame):
+        cap1 = cap.iloc[:, node]
+    else:
+        cap1 = cap
+    if isinstance(capF, pd.DataFrame):    
+        cap2 = capF.iloc[:, node]
+    else:
+        cap2 = capF
+    if isinstance(changepoints, pd.DataFrame):
+        changepoints1 = changepoints[:, node]
+    else:
+        changepoints1 = changepoints
+    if isinstance(n_changepoints, list):
+        n_changepoints1 = n_changepoints[node]
+    else:
+        n_changepoints1 = n_changepoints
+    ##
+    # Put the forecasts into a dictionary of dataframes
+    ##
+    with contextlib.redirect_stdout(open(os.devnull, "w")):
+        # Prophet related stuff
+        nodeToForecast = nodeToForecast.rename(columns = {nodeToForecast.columns[0] : 'ds'})
+        nodeToForecast = nodeToForecast.rename(columns = {nodeToForecast.columns[1] : 'y'})
+        if capF is None:
+            growth = 'linear'
+            m = Prophet(growth=growth,
+                        changepoints=changepoints1,
+                        n_changepoints=n_changepoints1,
+                        yearly_seasonality=yearly_seasonality,
+                        weekly_seasonality=weekly_seasonality,
+                        daily_seasonality=daily_seasonality,
+                        holidays=holidays,
+                        seasonality_prior_scale=seasonality_prior_scale,
+                        holidays_prior_scale=holidays_prior_scale,
+                        changepoint_prior_scale=changepoint_prior_scale,
+                        mcmc_samples=mcmc_samples,
+                        interval_width=interval_width,
+                        uncertainty_samples=uncertainty_samples)
+        else:
+            growth = 'logistic'
+            m = Prophet(growth=growth,
+                        changepoints=changepoints,
+                        n_changepoints=n_changepoints,
+                        yearly_seasonality=yearly_seasonality,
+                        weekly_seasonality=weekly_seasonality,
+                        daily_seasonality=daily_seasonality,
+                        holidays=holidays,
+                        seasonality_prior_scale=seasonality_prior_scale,
+                        holidays_prior_scale=holidays_prior_scale,
+                        changepoint_prior_scale=changepoint_prior_scale,
+                        mcmc_samples=mcmc_samples,
+                        interval_width=interval_width,
+                        uncertainty_samples=uncertainty_samples)
+            nodeToForecast['cap'] = cap1
+        m.fit(nodeToForecast)
+        future = m.make_future_dataframe(periods = h, freq = freq, include_history = include_history)
+        if capF is not None:
+            future['cap'] = cap2
+
+        ##
+        # Base Forecasts, Residuals, and MSE
+        ##
+        result = { 
+            "node": node,
+            "forecast": m.predict(future)
+        }
+        return result
 
 #%%
 def fitForecast(y, h, sumMat, nodes, method, freq, include_history, cap, capF, changepoints, n_changepoints, \
@@ -44,90 +137,56 @@ def fitForecast(y, h, sumMat, nodes, method, freq, include_history, cap, capF, c
         if method == 'FP':
             nForecasts = sum(list(map(sum, nodes)))+1
         
+        node_list = []
         for node in range(nForecasts):
-            nodeToForecast = pd.concat([y.iloc[:, [0]], y.iloc[:, node+1]], axis = 1)
-            if isinstance(cap, pd.DataFrame):
-                cap1 = cap.iloc[:, node]
-            else:
-                cap1 = cap
-            if isinstance(capF, pd.DataFrame):    
-                cap2 = capF.iloc[:, node]
-            else:
-                cap2 = capF
-            if isinstance(changepoints, pd.DataFrame):
-                changepoints1 = changepoints[:, node]
-            else:
-                changepoints1 = changepoints
-            if isinstance(n_changepoints, list):
-                n_changepoints1 = n_changepoints[node]
-            else:
-                n_changepoints1 = n_changepoints
+            node_params = {"node": node,
+                            "y": y,
+                            "cap": cap,
+                            "capF": capF,
+                            "changepoints": changepoints,
+                            "n_changepoints": n_changepoints,
+                            "yearly_seasonality": yearly_seasonality,
+                            "weekly_seasonality": weekly_seasonality,
+                            "daily_seasonality": daily_seasonality,
+                            "holidays": holidays,
+                            "seasonality_prior_scale": seasonality_prior_scale,
+                            "holidays_prior_scale": holidays_prior_scale,
+                            "changepoint_prior_scale": changepoint_prior_scale,
+                            "mcmc_samples": mcmc_samples,
+                            "interval_width": interval_width,
+                            "uncertainty_samples": uncertainty_samples,
+                            "h": h,
+                            "freq": freq,
+                            "include_history": include_history}
+            node_list.append(node_params)
+
+        print("Launching pool...")
+        with multiprocessing.Pool() as pool:
+            node_forecasts = pool.map(fitInvididualForecast, node_list)
+
+        for node_f in node_forecasts:
+            node = node_f ["node"]
+            forecastsDict[node] = node_f["forecast"]
+            resids[node] = y.iloc[:, node+1] - forecastsDict[node].yhat[:-h].values
+            mse[node] = np.mean(np.array(resids[node])**2)
             ##
-            # Put the forecasts into a dictionary of dataframes
+            # If logistic use exponential function, so that values can be added correctly
             ##
-            with contextlib.redirect_stdout(open(os.devnull, "w")):
-                # Prophet related stuff
-                nodeToForecast = nodeToForecast.rename(columns = {nodeToForecast.columns[0] : 'ds'})
-                nodeToForecast = nodeToForecast.rename(columns = {nodeToForecast.columns[1] : 'y'})
-                if capF is None:
-                    growth = 'linear'
-                    m = Prophet(growth=growth,
-                                changepoints=changepoints1,
-                                n_changepoints=n_changepoints1,
-                                yearly_seasonality=yearly_seasonality,
-                                weekly_seasonality=weekly_seasonality,
-                                daily_seasonality=daily_seasonality,
-                                holidays=holidays,
-                                seasonality_prior_scale=seasonality_prior_scale,
-                                holidays_prior_scale=holidays_prior_scale,
-                                changepoint_prior_scale=changepoint_prior_scale,
-                                mcmc_samples=mcmc_samples,
-                                interval_width=interval_width,
-                                uncertainty_samples=uncertainty_samples)
-                else:
-                    growth = 'logistic'
-                    m = Prophet(growth=growth,
-                                changepoints=changepoints,
-                                n_changepoints=n_changepoints,
-                                yearly_seasonality=yearly_seasonality,
-                                weekly_seasonality=weekly_seasonality,
-                                daily_seasonality=daily_seasonality,
-                                holidays=holidays,
-                                seasonality_prior_scale=seasonality_prior_scale,
-                                holidays_prior_scale=holidays_prior_scale,
-                                changepoint_prior_scale=changepoint_prior_scale,
-                                mcmc_samples=mcmc_samples,
-                                interval_width=interval_width,
-                                uncertainty_samples=uncertainty_samples)
-                    nodeToForecast['cap'] = cap1
-                m.fit(nodeToForecast)
-                future = m.make_future_dataframe(periods = h, freq = freq, include_history = include_history)
-                if capF is not None:
-                    future['cap'] = cap2
-                ##
-                # Base Forecasts, Residuals, and MSE
-                ##
-                forecastsDict[node] = m.predict(future)
-                resids[node] = y.iloc[:, node+1] - forecastsDict[node].yhat[:-h].values
-                mse[node] = np.mean(np.array(resids[node])**2)
-                ##
-                # If logistic use exponential function, so that values can be added correctly
-                ##
-                if capF is not None:
-                    forecastsDict[node].yhat = np.exp(forecastsDict[node].yhat)
-                if boxcoxT is not None:
-                    forecastsDict[node].yhat = inv_boxcox(forecastsDict[node].yhat, boxcoxT[node])
-                    forecastsDict[node].trend = inv_boxcox(forecastsDict[node].trend, boxcoxT[node])
-                    if "seasonal" in forecastsDict[node].columns.tolist():
-                        forecastsDict[node].seasonal = inv_boxcox(forecastsDict[node].seasonal, boxcoxT[node])
-                    if "daily" in forecastsDict[node].columns.tolist():
-                        forecastsDict[node].daily = inv_boxcox(forecastsDict[node].daily, boxcoxT[node])
-                    if "weekly" in forecastsDict[node].columns.tolist():
-                        forecastsDict[node].weekly = inv_boxcox(forecastsDict[node].weekly, boxcoxT[node])
-                    if "yearly" in forecastsDict[node].columns.tolist():
-                        forecastsDict[node].yearly = inv_boxcox(forecastsDict[node].yearly, boxcoxT[node])
-                    if "holidays" in forecastsDict[node].columns.tolist():
-                        forecastsDict[node].yearly = inv_boxcox(forecastsDict[node].yearly, boxcoxT[node])
+            if capF is not None:
+                forecastsDict[node].yhat = np.exp(forecastsDict[node].yhat)
+            if boxcoxT is not None:
+                forecastsDict[node].yhat = inv_boxcox(forecastsDict[node].yhat, boxcoxT[node])
+                forecastsDict[node].trend = inv_boxcox(forecastsDict[node].trend, boxcoxT[node])
+                if "seasonal" in forecastsDict[node].columns.tolist():
+                    forecastsDict[node].seasonal = inv_boxcox(forecastsDict[node].seasonal, boxcoxT[node])
+                if "daily" in forecastsDict[node].columns.tolist():
+                    forecastsDict[node].daily = inv_boxcox(forecastsDict[node].daily, boxcoxT[node])
+                if "weekly" in forecastsDict[node].columns.tolist():
+                    forecastsDict[node].weekly = inv_boxcox(forecastsDict[node].weekly, boxcoxT[node])
+                if "yearly" in forecastsDict[node].columns.tolist():
+                    forecastsDict[node].yearly = inv_boxcox(forecastsDict[node].yearly, boxcoxT[node])
+                if "holidays" in forecastsDict[node].columns.tolist():
+                    forecastsDict[node].yearly = inv_boxcox(forecastsDict[node].yearly, boxcoxT[node])
     ##
     # Now, Revise them
     ##
